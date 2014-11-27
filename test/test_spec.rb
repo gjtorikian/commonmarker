4d8c0cbd6b7fb6eefb863add703aaa1503837f6a
John MacFarlane <jgm@berkeley.edu>
2014-11-27 13:53:55 -0800

    Adde test/test_spec.rb.

---
+++ test/test_spec.rb
@@ -0,0 +1,24 @@
+require 'commonmarker'
+require 'minitest/autorun'
+require 'json'
+include CommonMarker
+
+class TestSpec < Minitest::Unit::TestCase
+  cases = JSON.parse(open("test/spec_tests.json", 'r').read)
+  cases.each do |testcase|
+    define_method("test_to_html_example_#{testcase['example']}") do
+      doc = Node.parse_string(testcase['markdown'])
+      actual = doc.to_html
+      doc.free
+      assert_equal actual, testcase['html']
+    end
+    define_method("test_html_renderer_example_#{testcase['example']}") do
+      doc = Node.parse_string(testcase['markdown'])
+      actual = HtmlRenderer.new.render(doc)
+      doc.free
+      assert_equal actual, testcase['html']
+    end
+  end
+end
+
+
