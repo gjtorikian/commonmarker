e736b19e6f9b5cfc94d29c0e8a889fb4d4089e86
Garen Torikian <gjtorikian@gmail.com>
2015-05-10 10:40:14 -0700

    Use proper .so extension for Linux

--- ext/commonmarker/extconf.rb
+++ ext/commonmarker/extconf.rb
@@ -3,6 +3,12 @@ require 'fileutils'
 require 'rbconfig'
 host_os = RbConfig::CONFIG['host_os']
 
+if host_os =~ /darwin|mac os/
+  ext = 'a'
+else
+  ext = 'so'
+end
+
 CMARK_DIR = File.expand_path(File.join(File.dirname(__FILE__), 'cmark'))
 CMARK_BUILD_DIR = File.join(CMARK_DIR, 'build')
 FileUtils.mkdir_p(CMARK_BUILD_DIR)
@@ -12,7 +18,7 @@ Dir.chdir(CMARK_BUILD_DIR) do
   system 'make'
 end
 
-$CFLAGS << " -fPIC -I#{CMARK_DIR}/src -I#{CMARK_BUILD_DIR}/src"
-$LOCAL_LIBS << "#{CMARK_BUILD_DIR}/src/libcmark.a"
+$CFLAGS << " -I#{CMARK_DIR}/src -I#{CMARK_BUILD_DIR}/src"
+$LOCAL_LIBS << "#{CMARK_BUILD_DIR}/src/libcmark.#{ext}"
 
 create_makefile('commonmarker/commonmarker')
