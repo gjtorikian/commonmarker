ea6158aefc79d25ce02ba45de029bceed3c69c44
Garen Torikian <gjtorikian@gmail.com>
2015-05-12 21:36:39 -0700

    Parse the string once

--- test/test_spec.rb
+++ test/test_spec.rb
@@ -5,14 +5,13 @@ class TestSpec < Minitest::Test
   cases = JSON.parse(open('test/spec_tests.json', 'r').read)
   cases.each do |testcase|
     # next unless testcase['example'] == 307
+    doc = Node.parse_string(testcase['markdown'])
     define_method("test_to_html_example_#{testcase['example']}") do
-      doc = Node.parse_string(testcase['markdown'])
       actual = doc.to_html
       doc.free
       assert_equal testcase['html'], actual, testcase['markdown']
     end
     define_method("test_html_renderer_example_#{testcase['example']}") do
-      doc = Node.parse_string(testcase['markdown'])
       actual = HtmlRenderer.new.render(doc)
       doc.free
       File.write('test.txt', testcase['html'])
