408915940b989c2ea7c502c1e9e38577c807602c
Garen Torikian <gjtorikian@gmail.com>
2015-05-20 21:16:00 -0700

    Add the skipping comment

--- test/test_spec.rb
+++ test/test_spec.rb
@@ -4,6 +4,7 @@ require 'json'
 class TestSpec < Minitest::Test
   cases = JSON.parse(open('test/spec_tests.json', 'r').read)
   cases.each do |testcase|
+    # next unless testcase['example'] == 420
     doc = Node.parse_string(testcase['markdown'])
 
     define_method("test_to_html_example_#{testcase['example']}") do
