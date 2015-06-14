779b46252df8661a3626f80890acb8ef2fdad2f4
Garen Torikian <gjtorikian@gmail.com>
2015-06-14 16:54:18 -0700

    Finish testing line breaks

--- test/test_linebreaks.rb
+++ test/test_linebreaks.rb
@@ -1,13 +1,19 @@
 require 'test_helper'
 
 class TestLinebreaks < Minitest::Test
-  def test_softbreak_no_spaces_as_hard
+  def test_hardbreak_no_spaces
+    doc = Node.parse_string("foo\nbaz")
+    assert_equal "<p>foo\nbaz</p>\n", doc.to_html
+
     doc = Node.parse_string("foo\nbaz", :hardbreaks)
-    assert_equal "<p>Hi <em>there</em></p>\n", doc.to_html
+    assert_equal "<p>foo<br />\nbaz</p>\n", doc.to_html(:hardbreaks)
   end
 
-  def test_softbreak_with_spaces_as_hard
-    doc = Node.parse_string("foo \n baz", :hardbreaks)
-    assert_equal "<p>Hi <em>there</em></p>\n", doc.to_html
+  def test_hardbreak_with_spaces
+    doc = Node.parse_string("foo  \nbaz")
+    assert_equal "<p>foo<br />\nbaz</p>\n", doc.to_html
+
+    doc = Node.parse_string("foo  \nbaz", :hardbreaks)
+    assert_equal "<p>foo<br />\nbaz</p>\n", doc.to_html(:hardbreaks)
   end
 end
