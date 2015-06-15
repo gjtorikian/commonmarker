9e74ad1794d6be535a2fe4f4a9aaa3f4a279e290
Garen Torikian <gjtorikian@gmail.com>
2015-06-14 17:50:52 -0700

    Fix smartpunct test generation

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,7 @@ require 'minitest/pride'
 
 include CommonMarker
 
-def open_spec_file(filename)
+def open_spec_file(filename, options = {})
   line_number = 0
   start_line = 0
   end_line = 0
@@ -16,9 +16,9 @@ def open_spec_file(filename)
   tests = []
 
   header_re = Regexp.new('#+ ')
+  filepath = File.join('ext', 'commonmarker', 'cmark', 'test', filename)
 
-  file = open(File.join('ext', 'commonmarker', 'cmark', 'test', filename), 'r').read
-  file.split("\n").each do |line|
+  File.readlines(filepath).each do |line|
     line_number += 1
     if state == 0 && header_re.match(line)
       headertext = line.sub(header_re, '').strip
@@ -30,8 +30,8 @@ def open_spec_file(filename)
         example_number += 1
         end_line = line_number
         tests << {
-          :markdown => markdown_lines.join('').gsub('→',"\t"),
-          :html => html_lines.join(''),
+          :markdown => markdown_lines.join('').gsub('→', "\t"),
+          :html => html_lines.join('').rstrip,
           :example => example_number,
           :start_line => start_line,
           :end_line => end_line,
@@ -43,9 +43,9 @@ def open_spec_file(filename)
       end
     elsif state == 1
       start_line = line_number - 1 if start_line == 0
-      markdown_lines << line
+      markdown_lines << "#{line}"
     elsif state == 2
-      html_lines << line.rstrip
+      html_lines << "#{line}"
     end
   end
 
--- test/test_smartpunct.rb
+++ test/test_smartpunct.rb
@@ -1,10 +1,10 @@
 require 'test_helper'
 
 class SmartPunctTest < Minitest::Test
-  smart_punct = open_spec_file('smart_punct.txt')
+  smart_punct = open_spec_file('smart_punct.txt', normalize: true)
 
   smart_punct.each do |testcase|
-    doc = Node.parse_string(testcase[:markdown], :smart)
+    doc = Node.parse_string(testcase[:markdown], [:smart])
 
     define_method("test_smart_punct_example_#{testcase[:example]}") do
       actual = doc.to_html.strip
