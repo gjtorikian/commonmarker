3fdf11c4e88abf4b4c910e3a191925b57bac0dfd
Garen Torikian <gjtorikian@gmail.com>
2015-07-06 23:45:29 -0700

    Force encoding conversions

--- test/test_encoding.rb
+++ test/test_encoding.rb
@@ -1,9 +1,10 @@
 require 'test_helper'
 
 class TestEncoding < Minitest::Test
+  # see http://git.io/vq4FR
   def test_encoding
     contents = File.read(File.join(FIXTURES_DIR, 'curly.md'))
     render = CommonMarker.render_html(contents, :smart)
-    assert_equal render.rstrip, "<p>This curly quote \\xE2\\x80\\x9Cmakes commonmarker throw an exception\\xE2\\x80\\x9D.</p>"
+    assert_equal render.rstrip, '<p>This curly quote “makes commonmarker throw an exception”.</p>'
   end
 end
