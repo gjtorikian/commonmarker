d0acc342ade8b2ebd5cf94db47e7ad0ba30b324a
Garen Torikian <gjtorikian@gmail.com>
2016-01-19 23:55:13 -0800

    Update to new test run style

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -22,32 +22,34 @@ def open_spec_file(filename, options = {})
 
   File.readlines(filepath).each do |line|
     line_number += 1
-    if state == 0 && header_re.match(line)
-      headertext = line.sub(header_re, '').strip
-    end
 
-    if line.strip == '.'
-      state = (state + 1) % 3
-      if state == 0
-        example_number += 1
-        end_line = line_number
-        tests << {
-          :markdown => markdown_lines.join('').gsub('→', "\t"),
-          :html => html_lines.join('').gsub('→', "\t").rstrip,
-          :example => example_number,
-          :start_line => start_line,
-          :end_line => end_line,
-          :section => headertext
-        }
-        start_line = 0
-        markdown_lines = []
-        html_lines = []
-      end
+    l = line.strip
+    if l == '`' * 32 + ' example'
+      state = 1
+    elsif l == '`' * 32
+      state = 0
+      example_number += 1
+      end_line = line_number
+      tests << {
+        :markdown => markdown_lines.join('').gsub('→', "\t"),
+        :html => html_lines.join('').gsub('→', "\t").rstrip,
+        :example => example_number,
+        :start_line => start_line,
+        :end_line => end_line,
+        :section => headertext
+      }
+      start_line = 0
+      markdown_lines = []
+      html_lines = []
+    elsif l == '.'
+      state = 2
     elsif state == 1
       start_line = line_number - 1 if start_line == 0
       markdown_lines << "#{line}"
     elsif state == 2
       html_lines << "#{line}"
+    elsif state == 0&& header_re.match(line)
+      headertext = line.sub(header_re, '').strip
     end
   end
 
