82f092e26a2e5d4a21f7956f7cf5791897c3bafb
Garen Torikian <gjtorikian@gmail.com>
2016-05-23 12:27:04 -0700

    Improve pathological testing

--- test/test_pathological_inputs.rb
+++ test/test_pathological_inputs.rb
@@ -5,13 +5,52 @@ def markdown(s)
   CommonMarker.render_doc(s).to_html
 end
 
-# Disabled by default
-# (these are the easy ones -- the evil ones are not disclosed)
-if ENV["BENCH"] then
-  class PathologicalInputsTest < Minitest::Benchmark
-    def setup
-    end
+# list of pairs consisting of input and a regex that must match the output.
+pathological = {
+  'nested strong emph' =>
+              [('*a **a ' * 65_000) + 'b' + (' a** a*' * 65_000),
+               Regexp.compile('(<em>a <strong>a ){65_000}b( a</strong> a</em>){65_000}')],
+  'many emph closers with no openers' =>
+               [('a_ ' * 65_000),
+                Regexp.compile('(a[_] ){64999}a_')],
+  'many emph openers with no closers' =>
+               [('_a ' * 65_000),
+                Regexp.compile('(_a ){64999}_a')],
+  'many link closers with no openers' =>
+               [('a]' * 65_000),
+                Regexp.compile('(a\]){65_000}')],
+  'many link openers with no closers' =>
+               [('[a' * 65_000),
+                Regexp.compile('(\[a){65_000}')],
+  'mismatched openers and closers' =>
+               [('*a_ ' * 50_000),
+                Regexp.compile('([*]a[_] ){49999}[*]a_')],
+  'link openers and emph closers' =>
+               [('[ a_' * 50_000),
+                Regexp.compile('(\[ a_){50000}')],
+  'hard link/emph case':
+               [('**x [a*b**c*](d)'),
+                Regexp.compile('\\*\\*x <a href=\'d\'>a<em>b</em><em>c</em></a>')],
+  'nested brackets':
+               [('[' * 50_000) + 'a' + (']' * 50_000),
+                Regexp.compile('\[{50000}a\]{50000}')],
+  'nested block quotes':
+               [(('> ' * 50_000) + 'a'),
+                Regexp.compile('(<blockquote>\n){50000}')],
+  'U+0000 in input':
+               [('abc\u0000de\u0000'),
+                Regexp.compile('abc\ufffd?de\ufffd?')]
+}
 
+pathological.each_pair do |name, description|
+  define_method("test_#{name}") do
+    input, regex = description
+    assert markdown(input)
+  end
+end
+
+if ENV["BENCH"] then
+  class PathologicalInputsPerformanceTest < Minitest::Benchmark
     def bench_pathological_1
       assert_performance_linear 0.99 do |n|
         star = '*'  * (n * 10)
@@ -28,7 +67,7 @@ if ENV["BENCH"] then
 
     def bench_pathological_3
       assert_performance_linear 0.99 do |n|
-        markdown(" [a]: #{ "A" * n }\n\n#{ "[a][]" * n }\n")
+        markdown(" [a]: #{'A' * n}\n\n#{'[a][]' * n}\n")
       end
     end
 
@@ -46,7 +85,7 @@ if ENV["BENCH"] then
 
     def bench_unbound_recursion
       assert_performance_linear 0.99 do |n|
-        markdown(("[" * n) + "foo" + ("](bar)" * n ))
+        markdown(('[' * n) + 'foo' + ('](bar)' * n))
       end
     end
   end
