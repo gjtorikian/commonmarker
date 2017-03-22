19ff6652e30d8d8f8572b3dbc759d09bfef1e5f7
Yuki Izumi <yuki@kivikakk.ee>
2017-03-22 11:40:54 +1100

    Add GITHUB_PRE_LANG renderer option

--- lib/commonmarker/config.rb
+++ lib/commonmarker/config.rb
@@ -18,6 +18,7 @@ module CommonMarker
       define :SOURCEPOS, (1 << 1)
       define :HARDBREAKS, (1 << 2)
       define :SAFE, (1 << 3)
+      define :GITHUB_PRE_LANG, (1 << 11)
     end
 
     def self.process_options(option, type)
