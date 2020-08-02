a201f0ed65059d111e5303e8ee1ef6777509d322
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2020-08-02 19:51:51 +1200

    Rework `extconf.rb` due to build failures in production.

--- ext/markly/extconf.rb
+++ ext/markly/extconf.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
+# Loads mkmf which is used to make makefiles for Ruby extensions
 require 'mkmf'
 
-$CFLAGS << ' -std=c99'
+$CFLAGS << " -O3 -std=c99"
 
-create_makefile('markly/markly')
+gem_name = File.basename(__dir__)
+extension_name = 'markly'
+
+# The destination:
+dir_config(extension_name)
+
+# Generate the makefile to compile the native binary into `lib`:
+create_makefile(File.join(gem_name, extension_name))
