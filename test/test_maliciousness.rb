89f48e6d34f1e160b04160cca9f08e2c8673342e
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2022-08-01 18:19:53 -0400

    typo

--- test/test_maliciousness.rb
+++ test/test_maliciousness.rb
@@ -2,51 +2,49 @@
 
 require "test_helper"
 
-module CommonMarker
-  class TestMaliciousness < Minitest::Test
-    def test_rendering_with_bad_type
-      assert_raises(TypeError) do
-        Commonmarker.to_html(nil)
-      end
-
-      assert_raises(TypeError) do
-        Commonmarker.to_html(123)
-      end
-
-      assert_raises(TypeError) do
-        Commonmarker.to_html([123])
-      end
-
-      assert_raises(TypeError) do
-        Commonmarker.to_html("foo \n baz", options: 123)
-      end
-
-      assert_raises(TypeError) do
-        Commonmarker.to_html("foo \n baz", options: :totes_fake)
-      end
-
-      assert_raises(TypeError) do
-        Commonmarker.to_html("foo \n baz", options: [])
-      end
-
-      assert_raises(TypeError) do
-        Commonmarker.to_html("foo \n baz", options: [23])
-      end
-
-      assert_raises(TypeError) do
-        Commonmarker.to_html("foo \n baz", options: nil)
-      end
-
-      assert_raises(TypeError) do
-        Commonmarker.to_html("foo \n baz", options: [:SMART, "totes_fake"])
-      end
+class TestMaliciousness < Minitest::Test
+  def test_rendering_with_bad_type
+    assert_raises(TypeError) do
+      Commonmarker.to_html(nil)
     end
 
-    def test_bad_options_value
-      err = assert_raises(TypeError) do
-        Commonmarker.to_html("foo \n baz", options: { parse: { smart: 111 } })
-      end
-      assert_equal("parse_options[:smart] must be a Boolean; got Integer", err.message)
+    assert_raises(TypeError) do
+      Commonmarker.to_html(123)
     end
+
+    assert_raises(TypeError) do
+      Commonmarker.to_html([123])
+    end
+
+    assert_raises(TypeError) do
+      Commonmarker.to_html("foo \n baz", options: 123)
+    end
+
+    assert_raises(TypeError) do
+      Commonmarker.to_html("foo \n baz", options: :totes_fake)
+    end
+
+    assert_raises(TypeError) do
+      Commonmarker.to_html("foo \n baz", options: [])
+    end
+
+    assert_raises(TypeError) do
+      Commonmarker.to_html("foo \n baz", options: [23])
+    end
+
+    assert_raises(TypeError) do
+      Commonmarker.to_html("foo \n baz", options: nil)
+    end
+
+    assert_raises(TypeError) do
+      Commonmarker.to_html("foo \n baz", options: [:SMART, "totes_fake"])
+    end
+  end
+
+  def test_bad_options_value
+    err = assert_raises(TypeError) do
+      Commonmarker.to_html("foo \n baz", options: { parse: { smart: 111 } })
+    end
+    assert_equal("parse_options[:smart] must be a Boolean; got Integer", err.message)
   end
 end
