d933ca134e7743221a5ac220564c76eae65bb266
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2023-03-01 12:46:57 -0500

    Add ability to load `tmtheme`s from a folder

--- ext/commonmarker/src/lib.rs
+++ ext/commonmarker/src/lib.rs
@@ -1,10 +1,17 @@
 extern crate core;
 
+use std::path::PathBuf;
+
+use ::syntect::highlighting::ThemeSet;
 use comrak::{
-    adapters::SyntaxHighlighterAdapter, markdown_to_html, markdown_to_html_with_plugins,
-    plugins::syntect::SyntectAdapter, ComrakOptions, ComrakPlugins,
+    adapters::SyntaxHighlighterAdapter,
+    markdown_to_html, markdown_to_html_with_plugins,
+    plugins::syntect::{SyntectAdapter, SyntectAdapterBuilder},
+    ComrakOptions, ComrakPlugins,
 };
-use magnus::{define_module, function, r_hash::ForEach, scan_args, Error, RHash, Symbol, Value};
+use magnus::{
+    define_module, exception, function, r_hash::ForEach, scan_args, Error, RHash, Symbol, Value,
+};
 
 mod options;
 use options::iterate_options_hash;
@@ -12,7 +19,8 @@ use options::iterate_options_hash;
 mod plugins;
 use plugins::{
     syntax_highlighting::{
-        fetch_syntax_highlighter_theme, SYNTAX_HIGHLIGHTER_PLUGIN_DEFAULT_THEME,
+        fetch_syntax_highlighter_path, fetch_syntax_highlighter_theme,
+        SYNTAX_HIGHLIGHTER_PLUGIN_DEFAULT_THEME,
     },
     SYNTAX_HIGHLIGHTER_PLUGIN,
 };
@@ -52,13 +60,75 @@ fn commonmark_to_html<'a>(args: &[Value]) -> Result<String, magnus::Error> {
         let adapter: SyntectAdapter;
 
         let theme = match rb_plugins.get(Symbol::new(SYNTAX_HIGHLIGHTER_PLUGIN)) {
-            Some(theme_val) => fetch_syntax_highlighter_theme(theme_val)?,
+            Some(syntax_highlighter_options) => {
+                fetch_syntax_highlighter_theme(syntax_highlighter_options)?
+            }
             None => SYNTAX_HIGHLIGHTER_PLUGIN_DEFAULT_THEME.to_string(), // no `syntax_highlighter:` defined
         };
 
-        if theme.is_empty() || theme == "none" {
+        let path = match rb_plugins.get(Symbol::new(SYNTAX_HIGHLIGHTER_PLUGIN)) {
+            Some(syntax_highlighter_options) => {
+                fetch_syntax_highlighter_path(syntax_highlighter_options)?
+            }
+            None => PathBuf::from("".to_string()), // no `syntax_highlighter:` defined
+        };
+
+        if !path.eq(&PathBuf::from("".to_string())) && !path.exists() {
+            return Err(Error::new(
+                exception::arg_error(),
+                format!("path does not exist"),
+            ));
+        }
+
+        if theme.is_empty() && path.exists() {
+            return Err(Error::new(
+                exception::arg_error(),
+                "`path` also needs `theme` passed into the `syntax_highlighter`",
+            ));
+        }
+        if path.exists() && !path.is_dir() {
+            return Err(Error::new(
+                exception::arg_error(),
+                "`path` needs to be a directory",
+            ));
+        }
+
+        if path.exists() {
+            let builder = SyntectAdapterBuilder::new();
+            let mut ts = ThemeSet::load_defaults();
+
+            match ts.add_from_folder(&path) {
+                Ok(_) => {}
+                Err(e) => {
+                    return Err(Error::new(
+                        exception::arg_error(),
+                        format!("failed to load theme set from path: {e}"),
+                    ));
+                }
+            }
+
+            ts.themes.get(&theme).ok_or_else(|| {
+                Error::new(
+                    exception::arg_error(),
+                    format!("theme `{}` does not exist", theme),
+                )
+            })?;
+
+            adapter = builder.theme_set(ts).theme(&theme).build();
+
+            syntax_highlighter = Some(&adapter);
+        } else if theme.is_empty() || theme == "none" {
             syntax_highlighter = None;
         } else {
+            ThemeSet::load_defaults()
+                .themes
+                .get(&theme)
+                .ok_or_else(|| {
+                    Error::new(
+                        exception::arg_error(),
+                        format!("theme `{}` does not exist", theme),
+                    )
+                })?;
             adapter = SyntectAdapter::new(&theme);
             syntax_highlighter = Some(&adapter);
         }
