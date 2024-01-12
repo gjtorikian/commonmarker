894fa78755a23f8ae62a4bf3fbd792908ae7af01
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2024-01-12 16:04:15 -0500

    get a test working

--- ext/commonmarker/src/node.rs
+++ ext/commonmarker/src/node.rs
@@ -1,5 +1,5 @@
 use comrak::nodes::NodeValue;
-use magnus::{method, Module, RModule};
+use magnus::{method, Module, RModule, Symbol};
 
 #[magnus::wrap(class = "Commonmarker::Node")]
 #[allow(dead_code)]
@@ -12,8 +12,43 @@ impl CommonmarkerNode {
         Ok(CommonmarkerNode { value })
     }
 
-    fn get_type(&self) -> &'static str {
-        "Commonmarker::Nodezzz"
+    fn get_type(&self) -> Symbol {
+        let type_str = match self.value {
+            NodeValue::Document => "document",
+            NodeValue::BlockQuote => "block_quote",
+            NodeValue::FootnoteDefinition(_) => "footnote_definition",
+            NodeValue::List(..) => "list",
+            NodeValue::DescriptionList => "description_list",
+            NodeValue::DescriptionItem(_) => "description_item",
+            NodeValue::DescriptionTerm => "description_term",
+            NodeValue::DescriptionDetails => "description_details",
+            NodeValue::Item(..) => "item",
+            NodeValue::CodeBlock(..) => "code_block",
+            NodeValue::HtmlBlock(..) => "html_block",
+            NodeValue::Paragraph => "paragraph",
+            NodeValue::Heading(..) => "heading",
+            NodeValue::ThematicBreak => "thematic_break",
+            NodeValue::Table(..) => "table",
+            NodeValue::TableRow(..) => "table_row",
+            NodeValue::TableCell => "table_cell",
+            NodeValue::Text(..) => "text",
+            NodeValue::SoftBreak => "softbreak",
+            NodeValue::LineBreak => "linebreak",
+            NodeValue::Image(..) => "image",
+            NodeValue::Link(..) => "link",
+            NodeValue::Emph => "emph",
+            NodeValue::Strong => "strong",
+            NodeValue::Code(..) => "code",
+            NodeValue::HtmlInline(..) => "html_inline",
+            NodeValue::Strikethrough => "strikethrough",
+            NodeValue::FrontMatter(_) => "frontmatter",
+            NodeValue::TaskItem { .. } => "taskitem",
+            NodeValue::Superscript => "superscript",
+            NodeValue::FootnoteReference(..) => "footnote_reference",
+            NodeValue::ShortCode(_) => "shortcode",
+        };
+
+        Symbol::new(type_str)
     }
 }
 
--- rakelib/test.rake
+++ rakelib/test.rake
@@ -5,7 +5,7 @@ require "rake/testtask"
 Rake::TestTask.new("test") do |t|
   t.libs << "lib"
   t.libs << "test"
-  t.pattern = "test/**/*_test.rb"
+  t.pattern = "test/**/parser_test.rb"
   t.verbose = true
   t.warning = false
 end
