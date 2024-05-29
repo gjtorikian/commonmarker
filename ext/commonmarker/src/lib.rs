22589cc068cb215ca5e19e59023ebaca17d8acdc
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2024-05-29 17:37:57 -0500

    Support plugins when processing node tree

--- ext/commonmarker/src/lib.rs
+++ ext/commonmarker/src/lib.rs
@@ -1,27 +1,15 @@
 extern crate core;
 
-use std::path::PathBuf;
-
-use ::syntect::highlighting::ThemeSet;
-use comrak::{
-    adapters::SyntaxHighlighterAdapter,
-    markdown_to_html, markdown_to_html_with_plugins, parse_document,
-    plugins::syntect::{SyntectAdapter, SyntectAdapterBuilder},
-    ComrakOptions, ComrakPlugins,
-};
-use magnus::{
-    define_module, exception, function, r_hash::ForEach, scan_args, Error, RHash, Symbol, Value,
-};
+use comrak::{markdown_to_html_with_plugins, parse_document, ComrakOptions};
+use magnus::{define_module, function, r_hash::ForEach, scan_args, Error, RHash, Symbol, Value};
 use node::CommonmarkerNode;
+use plugins::syntax_highlighting::construct_syntax_highlighter_from_plugin;
 
 mod options;
 use options::iterate_options_hash;
 
 mod plugins;
-use plugins::{
-    syntax_highlighting::{fetch_syntax_highlighter_path, fetch_syntax_highlighter_theme},
-    SYNTAX_HIGHLIGHTER_PLUGIN,
-};
+
 use typed_arena::Arena;
 
 mod node;
@@ -63,6 +51,32 @@ fn commonmark_to_html(args: &[Value]) -> Result<String, magnus::Error> {
     )?;
     let (rb_options, rb_plugins) = kwargs.optional;
 
+    let comrak_options = match format_options(rb_options) {
+        Ok(options) => options,
+        Err(err) => return Err(err),
+    };
+
+    let mut comrak_plugins = comrak::Plugins::default();
+
+    let syntect_adapter = match construct_syntax_highlighter_from_plugin(rb_plugins) {
+        Ok(Some(adapter)) => Some(adapter),
+        Ok(None) => None,
+        Err(err) => return Err(err),
+    };
+
+    match syntect_adapter {
+        Some(ref adapter) => comrak_plugins.render.codefence_syntax_highlighter = Some(adapter),
+        None => comrak_plugins.render.codefence_syntax_highlighter = None,
+    }
+
+    Ok(markdown_to_html_with_plugins(
+        &rb_commonmark,
+        &comrak_options,
+        &comrak_plugins,
+    ))
+}
+
+fn format_options(rb_options: Option<RHash>) -> Result<comrak::Options, magnus::Error> {
     let mut comrak_options = ComrakOptions::default();
 
     if let Some(rb_options) = rb_options {
@@ -72,101 +86,7 @@ fn commonmark_to_html(args: &[Value]) -> Result<String, magnus::Error> {
         })?;
     }
 
-    if let Some(rb_plugins) = rb_plugins {
-        let mut comrak_plugins = ComrakPlugins::default();
-
-        let syntax_highlighter: Option<&dyn SyntaxHighlighterAdapter>;
-        let adapter: SyntectAdapter;
-
-        let theme = match rb_plugins.get(Symbol::new(SYNTAX_HIGHLIGHTER_PLUGIN)) {
-            Some(syntax_highlighter_options) => {
-                match fetch_syntax_highlighter_theme(syntax_highlighter_options) {
-                    Ok(theme) => theme,
-                    Err(e) => {
-                        return Err(e);
-                    }
-                }
-            }
-            None => None, // no `syntax_highlighter:` defined
-        };
-
-        match theme {
-            None => syntax_highlighter = None,
-            Some(theme) => {
-                if theme.is_empty() {
-                    // no theme? uss css classes
-                    adapter = SyntectAdapter::new(None);
-                    syntax_highlighter = Some(&adapter);
-                } else {
-                    let path = match rb_plugins.get(Symbol::new(SYNTAX_HIGHLIGHTER_PLUGIN)) {
-                        Some(syntax_highlighter_options) => {
-                            fetch_syntax_highlighter_path(syntax_highlighter_options)?
-                        }
-                        None => PathBuf::from("".to_string()), // no `syntax_highlighter:` defined
-                    };
-
-                    if path.exists() {
-                        if !path.is_dir() {
-                            return Err(Error::new(
-                                exception::arg_error(),
-                                "`path` needs to be a directory",
-                            ));
-                        }
-
-                        let builder = SyntectAdapterBuilder::new();
-                        let mut ts = ThemeSet::load_defaults();
-
-                        match ts.add_from_folder(&path) {
-                            Ok(_) => {}
-                            Err(e) => {
-                                return Err(Error::new(
-                                    exception::arg_error(),
-                                    format!("failed to load theme set from path: {e}"),
-                                ));
-                            }
-                        }
-
-                        // check if the theme exists in the dir
-                        match ts.themes.get(&theme) {
-                            Some(theme) => theme,
-                            None => {
-                                return Err(Error::new(
-                                    exception::arg_error(),
-                                    format!("theme `{}` does not exist", theme),
-                                ));
-                            }
-                        };
-
-                        adapter = builder.theme_set(ts).theme(&theme).build();
-
-                        syntax_highlighter = Some(&adapter);
-                    } else {
-                        // no path? default theme lookup
-                        ThemeSet::load_defaults()
-                            .themes
-                            .get(&theme)
-                            .ok_or_else(|| {
-                                Error::new(
-                                    exception::arg_error(),
-                                    format!("theme `{}` does not exist", theme),
-                                )
-                            })?;
-                        adapter = SyntectAdapter::new(Some(&theme));
-                        syntax_highlighter = Some(&adapter);
-                    }
-                }
-            }
-        }
-        comrak_plugins.render.codefence_syntax_highlighter = syntax_highlighter;
-
-        Ok(markdown_to_html_with_plugins(
-            &rb_commonmark,
-            &comrak_options,
-            &comrak_plugins,
-        ))
-    } else {
-        Ok(markdown_to_html(&rb_commonmark, &comrak_options))
-    }
+    Ok(comrak_options)
 }
 
 #[magnus::init]
