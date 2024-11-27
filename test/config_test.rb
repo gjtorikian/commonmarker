916a5db8457dac087f9fd6e033fb2806e71a6058
ppworks <naoto5959@gmail.com>
2024-11-27 12:10:05 +0900

    Keep header_ids option as nil

--- test/config_test.rb
+++ test/config_test.rb
@@ -45,4 +45,39 @@ class ConfigTest < Minitest::Test
     # hardbreaks still work
     assert_equal("<p>aaaa<br />\nbbbb</p>\n", Commonmarker.to_html("aaaa\nbbbb", options: { render: { unsafe: false } }))
   end
+
+  def test_same_config_again
+    user_config = {
+      extension: {
+        header_ids: nil,
+      },
+    }
+
+    text = "# Heading-1"
+    expected = "<h1>Heading-1</h1>\n"
+
+    assert_equal(expected, Commonmarker.to_html(text, options: user_config))
+
+    # expect same result
+    assert_equal(expected, Commonmarker.to_html(text, options: user_config))
+  end
+
+  def test_render_and_to_html_with_same_config
+    user_config = {
+      extension: {
+        header_ids: nil,
+      },
+    }
+
+    text = "# Heading-1"
+    expected = "<h1>Heading-1</h1>\n"
+
+    doc = Commonmarker.parse(text, options: user_config)
+    # doc.walk do |node|
+    #   # do something
+    # end
+    doc.to_html(options: user_config)
+
+    assert_equal(expected, Commonmarker.to_html(text, options: user_config))
+  end
 end
