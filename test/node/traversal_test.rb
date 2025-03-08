a3912a9984c06053d5e822d1573c3f05192009d2
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2025-03-08 09:15:14 -0500

    no need

--- test/node/traversal_test.rb
+++ test/node/traversal_test.rb
@@ -1,11 +1,5 @@
 # frozen_string_literal: true
 
-# Released under the MIT License.
-# Copyright, 2023, by Samuel Williams.
-
-# require 'markly'
-# require 'markly'
-
 require "test_helper"
 
 class NodeTraversalTest < Minitest::Test
